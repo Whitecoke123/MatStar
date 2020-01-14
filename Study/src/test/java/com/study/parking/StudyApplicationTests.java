c3b5963aa7daf04e8c078f34a2c0902461c05bce
DESKTOP-VMRRM8Q\ojszz <ojszzang7942@gmail.com>
2020-01-14 20:05:20 +0900

    프로젝트 초기 생성 / 2020-01-14 / 오종석

---
+++ Study/src/test/java/com/study/parking/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
